e019ae4c8b1f38b8472688561e002f1083a6c34f
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-01-24 18:42:09 +0100

    snack-3

--- snack-3/index.php
+++ snack-3/index.php
@@ -2,14 +2,35 @@
 tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
 
 <?php
-$random=array();
-for($i=0; $i<15; $i++)
-{
-    if (!in_array($random)) {
-        $random[]=rand(0,100)."<br />";
-    }      
-}
-// echo var_dump($random);
-// var_dump ($random);
-print_r($random);
+
+$array = [];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+<body>
+
+    <h1>Random Numbers</h1>
+
+<?php 
+    for ($i = 0; $i < 15; $i++) {
+        $randNum = rand(1,100);
+        if (!in_array($randNum, $array)){
+           
+            $array[] = $randNum;
+        };
+        echo $randNum . "<br>";
+    }
+?>
+    
+</body>
+</html>
+
+
